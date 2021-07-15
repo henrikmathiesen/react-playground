21fc3a8cb3fd6bf11db169cce75e6b1e023ece76
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-07-15 20:15:14 +0200

    -sass and bootstrap, dokumentation, exploring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './sass/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -29,6 +29,8 @@ reportWebVitals();
 
   INFO:
 
+    ABSOLUTE IMPORTS
+  
     For absolute imports, create a jsconfig.json file in the root folder with:
     {
       "compilerOptions": {
@@ -40,6 +42,47 @@ reportWebVitals();
     import Home from Components/Home/Home
 
     There is also a .env file (see redux-repetition), but it doesnt seem to matter for this.
+
+
+    CSS
+
+    Not scoped like Angular
+
+
+    REACT ICONS
+
+    https://react-icons.github.io/react-icons
+
+
+    BOOTSTRAP THEMING
+
+    https://create-react-app.dev/docs/adding-a-sass-stylesheet          IMPORTANT, use npm install sass -S instead of node-sass
+    https://create-react-app.dev/docs/adding-bootstrap
+    https://getbootstrap.com/docs/5.0/customize/color/
+    https://flatuicolors.com/palette/defo
+
+
+    SASS IMPORTS
+
+    sass folder: 
+      index.scss imports settings that overrides bootstraps settings (for theming), and bootstrap
+      Here we can also place _mixins.scss
+
+    Components can import _settings.scss, _mixins.scss and also bootstraps files for variables and mixins
+    @import "~bootstrap/scss/functions";
+    @import "~bootstrap/scss/variables";
+    @import "~bootstrap/scss/mixins";
+
+    Would be trouble using a combined component-dependencies file, since that file should then include bootstrap settings
+    and would be used in index.scss preferebly, but all of bootstrap, including its settings file is already there.
+
+    // Basics.scss
+    @import '/src/sass/settings';
+    @import "~bootstrap/scss/functions";
+    @import "~bootstrap/scss/variables";
+
+    // Basics.js
+    import './Basics.scss';
 
 
 */
