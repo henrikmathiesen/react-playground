316c624464809130fdaf53d57d486b11810df141
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-08-07 10:33:27 +0200

    -loop and render list

--- src/Components/Basics/Basics.js
+++ src/Components/Basics/Basics.js
@@ -2,7 +2,7 @@ import BasicChild from './BasicChild';
 
 function Basics() {
 
-    const onSomeBtnClick = () => { 
+    const onSomeBtnClick = () => {
         console.log('btn clicked');
     };
 
@@ -16,11 +16,34 @@ function Basics() {
         b: 'b'
     }
 
+    const data = [
+        {
+            id: 1,
+            text: 'Item 1'
+        },
+        {
+            id: 2,
+            text: 'Item 2'
+        },
+        {
+            id: 3,
+            text: 'Item 3'
+        }
+    ];
+
 
     return (
         <div>
             <h2>Basics</h2>
             <BasicChild user={user} config={config} onSomeBtnClick={onSomeBtnClick} />
+
+            <div className="mt-2">
+                <ul>
+                    {
+                        data.map(v => <li key={v.id}>{v.text}</li>)
+                    }
+                </ul>
+            </div>
         </div>
     );
 }
