86a441d4a4f11625ece312f2de97362d9be0103a
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-09-26 15:25:49 +0200

    -useState

--- src/Components/Hooks/Lab/FirstHookSetState.js
+++ src/Components/Hooks/Lab/FirstHookSetState.js
@@ -1,9 +1,39 @@
-function FirstHookSetState() { 
+import { useState } from 'react';
+
+function FirstHookSetState() {
+
+    /*
+
+        const results = useState('');               // Set the initial value of the state. It returns an array, and the 2 first indexes is of interest to us
+        const inputText = results[0];               // A reference to state itself (out data model), readonly
+        const setInputText = results[1]             // The setter function we call to update the state
+
+        This can be refactored to:
+
+    */
+
+    const [inputText, setInputText] = useState('');
+    const [historyList, setHistoryList] = useState([]);
+
+    function onInputChange(e) { 
+        setInputText(e.target.value);
+        setHistoryList([...historyList, e.target.value]);
+    }
 
     return (
         <div className="row">
             <div className="col-md-8 col-lg-6">
-                <input className="form-control" onChange={ (e) => console.log('works') } />
+                <div>
+                    <input className="form-control" onChange={onInputChange} />
+                </div>
+                <div className="mt-2">
+                    {inputText}
+                </div>
+                <div className="mt-2">
+                    <ol>
+                        {historyList.map((hl, i) => <li key={i}>{hl}</li>)}
+                    </ol>
+                </div>
             </div>
         </div>
     );
