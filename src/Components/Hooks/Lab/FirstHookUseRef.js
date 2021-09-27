7a262890418b3e5c0cc652dd3c70599bd8f991f2
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-09-27 18:30:48 +0200

    -todos

--- src/Components/Hooks/Lab/FirstHookUseRef.js
+++ src/Components/Hooks/Lab/FirstHookUseRef.js
@@ -2,6 +2,15 @@ import { useState } from 'react';
 
 function FirstHookUseRef() {
 
+    /* 
+    
+        Primarily used to allow access directly to an element in the DOM.
+
+
+        TODO: use this for placeholder images (shared component)
+    
+    */
+
     return(
         <div className="row">
             <div className="col-md-8 col-lg-6">
