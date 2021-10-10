fbbb2586dc653e676ad51b7a8356e8b680b95f4c
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-10-10 10:53:20 +0200

    -docs

--- src/Components/Hooks/Lab/FirstHookUseState.js
+++ src/Components/Hooks/Lab/FirstHookUseState.js
@@ -5,7 +5,7 @@ function FirstHookUseState() {
     /*
 
         const results = useState('');               // Set the initial value of the state. It returns an array, and the 2 first indexes is of interest to us
-        const inputText = results[0];               // A reference to state itself (out data model), readonly
+        const inputText = results[0];               // A reference to state itself (the data model), readonly
         const setInputText = results[1]             // The setter function we call to update the state
 
         This can be refactored to:
