f53990dfc335603525ac72d12e22cff4773091c0
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-10-10 10:19:14 +0200

    -useEffect labbing

--- src/Components/Hooks/Lab/FirstHookUseEffect.js
+++ src/Components/Hooks/Lab/FirstHookUseEffect.js
@@ -1,4 +1,28 @@
+import { useEffect } from 'react';
+
 function FirstHookUseEffect() {
+
+    /* 
+    
+        We can think about the hook UseEffect as being very similiar to componentDidMount, componentDidUpdate, componentWillUnmount in React class component
+
+
+        REACT CLASS COMPONENT               REACT FUNCTIONAL COMPONENT
+        
+        componentDidMount                   useEffect(() => { ... })
+        componentDidUpdate                  useEffect(() => { ... })                                // Some conditions do apply
+        componentWillUnmount                useEffect(() => { ... return () => { ... } })           // The return function gets called
+    
+    */
+
+    useEffect(() => {
+        console.log('in use effect');
+
+        return () => { 
+            console.log('in use effect cleanup');
+        };
+    }, []);
+    // [] , a dependecy array (useState variables) (optional, could be empty, could contain dependencies), for re render the component if those dependecies changes (and useEffect is called again)
 
     return (
         <div className="row">
