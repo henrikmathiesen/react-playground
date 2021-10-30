7d04f56666dfd5c94ab113aebbdb56f250e10224
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-10-30 17:01:05 +0200

    -docs

--- src/Components/Hooks/Lab/FirstHookUseEffect.js
+++ src/Components/Hooks/Lab/FirstHookUseEffect.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 function FirstHookUseEffect() {
 
@@ -15,19 +16,31 @@ function FirstHookUseEffect() {
     
     */
 
+    const [isClicked, setIsClicked] = useState(false);
+
+    const onBtnClick = () => setIsClicked(!isClicked);
+
     useEffect(() => {
         console.log('in use effect');
 
         return () => { 
             console.log('in use effect cleanup');
         };
-    }, []);
-    // [] , a dependecy array (useState variables) (optional, could be empty, could contain dependencies), for re render the component if those dependecies changes (and useEffect is called again)
+    }, [isClicked]);
+    // [] , a dependecy array (useState variables)
+    // If left out, useEffect, and return function, is called when this component is rendered and on all uppdates
+    // If left empty, useEffect (first function) is called only once when this component is first rendered
+    // If populated with values, useEffect, and return function, is called when those values changes
+    // The return function is always called when this component unmounts
 
     return (
         <div className="row">
             <div className="col-md-8 col-lg-6">
                 Hi from FirstHookUseEffect
+
+                <div className="mt-4">
+                    <button className={`btn btn-secondary ${isClicked && 'toggle-clicked'}`} onClick={onBtnClick}>Click me</button>
+                </div>
             </div>
         </div>
     );
